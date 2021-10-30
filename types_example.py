bbae4c1b9d83fdc9b4f4188efbe0d03913f22440
Frederik Dangel <frederik.dangel@inovex.de>
2021-10-30 13:33:47 +0200

    Type
    Iterating over different types

---
+++ types_example.py
@@ -0,0 +1,13 @@
+# duck typing example
+from typing import Iterable
+
+
+def print_items(items: Iterable):
+    for item in items:
+        print(item)
+
+
+print_items([1,2,3])
+print_items({4, 5, 6})
+print_items({"A": 1, "B": 2, "C": 3})
+print_items({"A": 1, "B": 2, "C": 3}.values())
